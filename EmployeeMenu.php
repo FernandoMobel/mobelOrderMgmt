cb2e64f6d547cb829570ca437511a4c9f922cae2
FernandoMobel <68559367+FernandoMobel@users.noreply.github.com>
2020-08-04 22:10:50 -0700

    Filter functionality is fixed

--- EmployeeMenu.php
+++ EmployeeMenu.php
@@ -51,12 +51,28 @@ function saveOrder(objectID,OID){
 				console.log(OID);
 }
 
-
+function getOrderID(OID){
+	$('#searchOrderBtn').empty();
+	OID = OID.replace(/\s+/g, '');
+	if (OID.trim()){		
+		console.log(OID);		
+		myData = { mode: "getOrderID", id: OID.trim(), value: OID.trim()};
+		console.log(myData);
+		$.post("EmployeeMenuSettings.php",
+			myData, 
+		       function(data, status, jqXHR) {
+						$('#searchOrderBtn').append(data);
+						console.log(data);            	    
+		        });
+		
+	}else{
+		$('#searchOrderBtn').empty();
+	}
+	
+}
 
 function saveEmployeeSettings(objectID){
-	//$(".collapse").collapse('toggle');
 	var arr = $("#"+objectID).val();
-	//console.log(arr);
 	if(arr.length==0){
 		 arr = ["1","2","3","4","5","6","7","8","9","10"]; 
 	}
@@ -133,68 +149,77 @@ if($GLOBALS['$result2']-> num_rows >0){
 echo "<br/><div class=\"container\">";
     ?>
     <div id="orderView">
-		<p>
-			<a class="btn btn-primary btn-block btn-sm" data-toggle="collapse" href="#collapseExample" role="button" aria-expanded="false" aria-controls="collapseExample">
-				Filters
-			  </a>
-		</p>
-		<div class="collapse" id="collapseExample">
-			<?php						
-			/*Table Filters Start*/
-			//Getting all states
-			opendb2("select * from state order by position asc");
-			echo "<select id=\"stateFilter\" onchange=\"saveEmployeeSettings('stateFilter')\" placeholder=\"Status\" multiple>";
-			//Building filter
-			if($GLOBALS['$result2']->num_rows > 0){			
-				foreach ($GLOBALS['$result2'] as $row2) {	
-					foreach ($state_ar as &$value) {
-						$selected = ">";
-						if($value==$row2['id']){
-							$selected = "selected>";
-							break;
+		<div class="container">
+			<div class="row">
+				<div class="col-5 my-1">
+					<a class="btn btn-primary btn-sm" data-toggle="collapse" href="#collapse1" role="button" aria-expanded="false" aria-controls="collapse1">Filter
+						<svg width="1.5em" height="1.5em" viewBox="0 0 16 16" class="bi bi-funnel" fill="currentColor">
+						  <path fill-rule="evenodd" d="M1.5 1.5A.5.5 0 0 1 2 1h12a.5.5 0 0 1 .5.5v2a.5.5 0 0 1-.128.334L10 8.692V13.5a.5.5 0 0 1-.342.474l-3 1A.5.5 0 0 1 6 14.5V8.692L1.628 3.834A.5.5 0 0 1 1.5 3.5v-2zm1 .5v1.308l4.372 4.858A.5.5 0 0 1 7 8.5v5.306l2-.666V8.5a.5.5 0 0 1 .128-.334L13.5 3.308V2h-11z"/>
+						</svg><!--Filters-->
+					</a>
+					<div class="collapse" id="collapse1">
+						<label>Status</label>
+						<?php						
+						/*Table Filters Start*/
+						//Getting all states
+						opendb2("select * from state order by position asc");
+						echo "<select id=\"stateFilter\" onchange=\"saveEmployeeSettings('stateFilter')\" placeholder=\"Status\" multiple>";
+						//Building filter
+						if($GLOBALS['$result2']->num_rows > 0){			
+							foreach ($GLOBALS['$result2'] as $row2) {	
+								foreach ($state_ar as &$value) {
+									$selected = ">";
+									if($value==$row2['id']){
+										$selected = "selected>";
+										break;
+									}
+								}
+								echo "<option value=\"".$row2['id']."\" ".$selected.$row2['name']."</option>" ;
+							}
 						}
-					}
-					echo "<option value=\"".$row2['id']."\" ".$selected.$row2['name']."</option>" ;
-				}
-			}
-			echo "</select>";
-			//echo $state_ar;
-			?>
-		</div>				
-				
-			</td>
+						echo "</select>";
+						?>					
+					</div>
+				</div>
+				<div id="searchOrderBtn" class="col">
+				</div>
+				<div class= "col-3 my-1">					
+					<input id="openOrder" class="form-control" type="number" onkeyup="getOrderID(this.value)" placeholder="Search Order ID">					
+				</div>
+			</div>
 		</div>
-		<table id="example" class="table table-bordered table-hover table-sm" style="width:100%" >
-		<thead class="thead-dark">
-			<tr>
-				
-			</tr>
-			<tr>
-				<th>OID</th>
-				<th>Tag Name</th>
-				<th>PO</th>			
-				<th>Status</th>
-				<!--th >Update Status</th-->
-			</tr>
-		</thead>
-		<tfoot class="thead-dark">
-			<tr>
-				<th>OID</th>
-				<th>Tag Name</th>
-				<th>PO</th>
-				<th>Status</th>
-				<!--th >Update Status</th-->
-			</tr>
-		</tfoot>
-		<tbody id="orders">
-		<?php
+		<hr style="height:1px;border-width:0;color:gray;background-color:gray">
+	</div>
+	<table id="example" class="table" style="width:100%" >
+	<thead class="thead-light">
+		<tr>
+			
+		</tr>
+		<tr>
+			<th>OID</th>
+			<th>Tag Name</th>
+			<th>PO</th>			
+			<th>Status</th>
+			<!--th >Update Status</th-->
+		</tr>
+	</thead>
+	<tfoot class="thead-light">
+		<tr>
+			<th>OID</th>
+			<th>Tag Name</th>
+			<th>PO</th>
+			<th>Status</th>
+			<!--th >Update Status</th-->
+		</tr>
+	</tfoot>
+	<tbody id="orders">
+	<?php
 if($GLOBALS['$result']->num_rows > 0){
 		foreach ($GLOBALS['$result'] as $row) {
 			echo "<tr>";
 			echo "<td><b><a href=\"Order.php?OID=" . $row['oid'] . "\">".$row['oid']."</b></td>";
-			echo "<td>" . $row['tagName'] . "</td>";
-			//echo "<td>" . $row['status'] . "</td>";
-			echo "<td>" . $row['po'] . "</td>";	
+			echo "<td><a href=\"Order.php?OID=" . $row['oid'] . "\">" . $row['tagName'] . "</td>";
+			echo "<td><a href=\"Order.php?OID=" . $row['oid'] . "\">" . $row['po'] . "</td>";	
 			echo "<td>";
 			echo "<select onchange=\"saveOrder('state','" . $row['oid'] . "');\" id=\"state".$row['oid']."\" >";
 			opendb2("select * from state order by position asc");
@@ -232,16 +257,8 @@ if($GLOBALS['$result']->num_rows > 0){
 <?php include 'includes/foot.php';?>
 <script>
   
- $(document).ready(function(){
-
- var multipleCancelButton = new Choices('#stateFilter', {
- removeItemButton: true,
- maxItemCount:10,
- searchResultLimit:5,
- renderChoiceLimit:10
- });
-
-
+ $(document).ready(function(){	
+	//$('#searchOrderBtn').hide();
  });
 
 </script>
