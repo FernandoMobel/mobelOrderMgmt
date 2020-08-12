ee012de87f2f82024d8b76f80f7ca40d44a9b7d9
FernandoMobel <68559367+FernandoMobel@users.noreply.github.com>
2020-08-12 15:34:56 -0700

    Update selected value

--- EmployeeMenu.php
+++ EmployeeMenu.php
@@ -163,8 +163,9 @@ echo "<br/><div class=\"container\">";
 						//Building filter
 						if($GLOBALS['$result2']->num_rows > 0){			
 							foreach ($GLOBALS['$result2'] as $row2) {	
+								$selected = ">";
 								foreach ($state_ar as &$value) {
-									$selected = ">";
+									
 									if($value==$row2['id']){
 										$selected = "selected>";
 										break;
