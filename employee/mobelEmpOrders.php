81e5c5cf942eea112537e86435df40ececedd685
FernandoMobel <68559367+FernandoMobel@users.noreply.github.com>
2020-11-30 09:33:07 -0800

    Disable pint ajax response and calculations

--- employee/mobelEmpOrders.php
+++ employee/mobelEmpOrders.php
@@ -128,7 +128,6 @@ function updateDetail(rid,col,val){
 	myData, 
 	function(data, status, jqXHR) { 
 			//console.log(jqXHR['responseText']);
-			
 			$("#"+col).css("border-color", "#00b828");
 		});
 }
@@ -247,13 +246,17 @@ function setPrevState(){
 							<input id="openOrder" class="form-control" type="number" min="1" onkeyup="getOrderID(this.value)" placeholder="Open order by ID">					
 						</div>											
 					</div>
-					<div class="col-sm-6 col-lg-1 form-check d-flex justify-content-arround">
-						<label class="form-check-label" for="lockStatus">
-							<svg width="1.5em" height="1.5em" viewBox="0 0 16 16" class="bi bi-lock text-primary" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
-							  <path fill-rule="evenodd" d="M11.5 8h-7a1 1 0 0 0-1 1v5a1 1 0 0 0 1 1h7a1 1 0 0 0 1-1V9a1 1 0 0 0-1-1zm-7-1a2 2 0 0 0-2 2v5a2 2 0 0 0 2 2h7a2 2 0 0 0 2-2V9a2 2 0 0 0-2-2h-7zm0-3a3.5 3.5 0 1 1 7 0v3h-1V4a2.5 2.5 0 0 0-5 0v3h-1V4z"/>
-							</svg>
-						</label>
-						<input type="checkbox" class="form-check-input" id="lockStatus" checked>
+					<div class="col-sm-6 col-lg-1 form-check d-flex flex-row-reverse pr-0">
+						<div>
+							<label class="form-check-label" for="lockStatus">
+								<svg width="1.5em" height="1.5em" viewBox="0 0 16 16" class="bi bi-lock text-primary" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+								  <path fill-rule="evenodd" d="M11.5 8h-7a1 1 0 0 0-1 1v5a1 1 0 0 0 1 1h7a1 1 0 0 0 1-1V9a1 1 0 0 0-1-1zm-7-1a2 2 0 0 0-2 2v5a2 2 0 0 0 2 2h7a2 2 0 0 0 2-2V9a2 2 0 0 0-2-2h-7zm0-3a3.5 3.5 0 1 1 7 0v3h-1V4a2.5 2.5 0 0 0-5 0v3h-1V4z"/>
+								</svg>
+							</label>
+						</div>
+						<div>
+							<input type="checkbox" class="form-check-input" id="lockStatus" checked>
+						</div>
 					</div>
 				</div>
 			</div>
